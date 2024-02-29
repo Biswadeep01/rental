507f2f1ca0711c9748add763869c7fbbf433907b
Biswadeep Mukhopadhyay <biswadeep.mukhopadhyay00@gmail.com>
2024-03-01 02:41:56 +0530

    Updated Hero section

--- src/components/UI/HeroSlider.jsx
+++ src/components/UI/HeroSlider.jsx
@@ -1,63 +1,32 @@
 import React from "react";
-
-import Slider from "react-slick";
+import image1 from "../../assets/all-images/hero/Mercedes.png"
+import image2 from "../../assets/all-images/hero/wrangler.png"
+import Typewriter from "../UI/Typewriter";
 import { Container } from "reactstrap";
 import { Link } from "react-router-dom";
 
 import "../../styles/hero-slider.css";
 
 const HeroSlider = () => {
-  const settings = {
-    fade: true,
-    speed: 2000,
-    autoplaySpeed: 2500,
-    infinite: true,
-    autoplay: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    pauseOnHover: false,
-  };
   return (
-    <Slider {...settings} className="hero__slider">
-      <div className="slider__item slider__item-01 mt0">
+    <div className="hero__slider">
+      <div className="slider__item">
         <Container>
-          <div className="slider__content ">
-            <h4 className="text-light mb-3">Book Now</h4>
-            <h1 className="text-light mb-4">Just Rent a Car</h1>
-
-            <button className="btn reserve__btn mt-4">
-              <Link to="/fleet">Reserve Now</Link>
-            </button>
+          <div className="slider__content">
+            <img src={image2} className= "image2" alt="" />
+            <div className="parallelogram"></div>
+            <div className="hero-content">
+              <h1 className="text-light mb-1"><Typewriter text="Just" delay={100}/></h1>
+              <h4 className="text-light mb-2"><Typewriter text="Rent a Car" delay={200}/></h4>
+              <button className="btn reserve__btn mt-2.5">
+                <Link to="/fleet">Reserve Now</Link>
+              </button>
+            </div>
+            <img src={image1} className= "image1" alt="" />
           </div>
         </Container>
       </div>
-
-      <div className="slider__item slider__item-02 mt0">
-        <Container>
-          <div className="slider__content ">
-            <h4 className="text-light mb-3">Book Now</h4>
-            <h1 className="text-light mb-4">Just Rent a Car</h1>
-
-            <button className="btn reserve__btn mt-4">
-              <Link to="/fleet">Reserve Now</Link>
-            </button>
-          </div>
-        </Container>
-      </div>
-
-      <div className="slider__item slider__item-03 mt0">
-        <Container>
-          <div className="slider__content ">
-            <h4 className="text-light mb-3">Book Now</h4>
-            <h1 className="text-light mb-4">Just Rent a Car</h1>
-
-            <button className="btn reserve__btn mt-4">
-              <Link to="/fleet">Reserve Now</Link>
-            </button>
-          </div>
-        </Container>
-      </div>
-    </Slider>
+    </div>
   );
 };
 
