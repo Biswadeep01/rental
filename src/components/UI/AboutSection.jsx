2f5ad7bffc5f711f1623fae442bad000f883d607
Bishal Mukherjee <bishalmukherjee448@gmail.com>
2024-08-22 00:52:12 +0530

    Renaming First Rent-A-Car to Just Rent-A-Car

--- src/components/UI/AboutSection.jsx
+++ src/components/UI/AboutSection.jsx
@@ -20,19 +20,34 @@ const AboutSection = ({ aboutClass }) => {
               <h4 className="section__subtitle">About Us</h4>
               <h2 className="section__title">Welcome to car rent service</h2>
               <p className="section__description" style={{ fontWeight: 500 }}>
-              Welcome to First Rent-A-Car. When you need a convenient, friendly car rental service, our company is here to make your stay more pleasant.
-              Our great service includes complimentary pick up and drop off, and we are located near the airport so you can be on your way without delay. We honor Veteran and senior citizen discounts. 
-              Additionally, we apply discounts for rentals that’s six days and more. Our professionally detailed cars are safe, comfortable way to get around town. Whether you want a compact or mid-size car, SUV, minivan, or jeep, we offer free unlimited mileage on all our automatic, air-conditioned vehicles. 
-              Our efficient check-in and checkout  services guarantee that you’ll lose no time procuring and returning your rental car. Always buckle up and keep your speed to less than 35 mph.
-              Most importantly, enjoy your stay in sunny  St. Thomas and remember, in St. Thomas, left is right and right is wrong.
-              We drive on the side where you can pick the bush :
+                Welcome to Just Rent-A-Car. When you need a convenient, friendly
+                car rental service, our company is here to make your stay more
+                pleasant. Our great service includes complimentary pick up and
+                drop off, and we are located near the airport so you can be on
+                your way without delay. We honor Veteran and senior citizen
+                discounts. Additionally, we apply discounts for rentals that’s
+                six days and more. Our professionally detailed cars are safe,
+                comfortable way to get around town. Whether you want a compact
+                or mid-size car, SUV, minivan, or jeep, we offer free unlimited
+                mileage on all our automatic, air-conditioned vehicles. Our
+                efficient check-in and checkout services guarantee that you’ll
+                lose no time procuring and returning your rental car. Always
+                buckle up and keep your speed to less than 35 mph. Most
+                importantly, enjoy your stay in sunny St. Thomas and remember,
+                in St. Thomas, left is right and right is wrong. We drive on the
+                side where you can pick the bush :
               </p>
             </div>
           </Col>
 
           <Col lg="5" md="5">
             <div className="about__img">
-              <img src={aboutImg} alt="" className="w-100" style={{marginTop: "80px"}}/>
+              <img
+                src={aboutImg}
+                alt=""
+                className="w-100"
+                style={{ marginTop: "80px" }}
+              />
             </div>
           </Col>
         </Row>
